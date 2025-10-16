eae9d668848611210e77437633f78142caf61fd9
54MMYYY <samsonsgeorge2003@gmail.com>
2025-10-16 23:46:18 +0400

    Astronaut Model added with Orbital Controls

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,11 +1,20 @@
+import { Canvas } from "@react-three/fiber";
 import HeroText from "../components/HeroText";
 import ParallaxBg from "../components/ParallaxBg";
+import { Astronaut } from "../components/Astronaut";
+import { OrbitControls } from "@react-three/drei";
 
 const Hero = () => {
   return (
     <section className="flex items-start justify-center md:items-start md:justify-start min-h-screen overflow-hidden c-space">
         <HeroText />
         <ParallaxBg />
+        <figure className="absolute inset-0" style={{width: "100vw", height: "100vh "}}>
+          <Canvas>
+            <Astronaut />
+            <OrbitControls />
+          </Canvas>
+        </figure>
     </section>
   );
 };
